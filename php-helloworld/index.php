8971fcafe5c28ed42c1d6ebcdb5960a76b9bae37
Student User <student@workstation.lab.example.com>
2020-05-13 14:27:45 +0000

    update effettuato 2

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! php version is  . PHP_VERSION .  "My name is luigi zuccarelli "\n";
+print "Hello, World! php version is"  . PHP_VERSION .  "\n" "My name is luigi zuccarelli";
 ?>
